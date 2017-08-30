073428ce46ee9b4035bf42b6b430900532e5e680
sx5486510 <sx5486510@126.com>
2017-08-30 16:32:42 +0800

    1

--- src/main.cc
+++ src/main.cc
@@ -5,8 +5,8 @@
 #include <stdint.h>
 #include <string>
 #include <algorithm>
-#include "cryptonote_core/cryptonote_basic.h"
-#include "cryptonote_core/cryptonote_format_utils.h"
+#include "cryptonote_core/CryptoNoteBasic.h"
+#include "cryptonote_core/CryptoNoteFormatUtils.h"
 #include "cryptonote_protocol/blobdatatype.h"
 #include "crypto/crypto.h"
 #include "crypto/hash.h"
