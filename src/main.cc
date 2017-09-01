fcd56710d8e70383dbdf2b852d368c4b2c605ab9
sx5486510 <sx5486510@126.com>
2017-09-01 13:24:39 +0800

    1

--- src/main.cc
+++ src/main.cc
@@ -44,11 +44,11 @@ using namespace Common;
 
 bool parse_and_validate_block_from_blob(const std::string& b_blob, Block& b)
 {
-	std::cout << "1111" << std::endl
+	std::cout << "1111" << std::endl;
 	BinaryArray blob = fromHex(b_blob);
-	std::cout << "22222" << std::endl
+	std::cout << "22222" << std::endl;
 	bool r = fromBinaryArray(b, blob);
-	std::cout << "33333" << std::endl
+	std::cout << "33333" << std::endl;
 	CHECK_AND_ASSERT_MES(r, false, "Failed to parse Block from blob");
 	return true;
 }
